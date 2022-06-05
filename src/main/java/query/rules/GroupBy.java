b823ba79b9cbcdaa5789523a57d4291c52c8df25
tamaratomic <ttomic11320rn@raf.rs>
2022-06-05 21:27:54 +0200

    group by

--- src/main/java/query/rules/GroupBy.java
+++ src/main/java/query/rules/GroupBy.java
@@ -3,6 +3,7 @@ package query.rules;
 import query.Rule;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class GroupBy implements Rule {
@@ -13,6 +14,24 @@ public class GroupBy implements Rule {
 
     @Override
     public String check(List<String> query, Map<String, List<String>> map, Object object) {
+
+        if(map.get("SELECT").size() > 1){
+            for(String s : map.get("SELECT")){
+                String st = s.toUpperCase();
+                //COUNT, AVG,
+                //MIN, MAX i SUM.
+                if(st.contains("SUM") || st.contains("COUNT") || st.contains("AVG")
+                        || st.contains("MIN") || st.contains("MAX")){
+                    if(!map.containsKey("GROUP BY")){
+                        System.out.println("POTREBAN GROUP BY");
+                        return null;
+                    }
+                }
+            }
+        }
+
+
+
         return null;
     }
 }
